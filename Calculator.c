5e1bb7f28cd357eaea1735ea6957cc34f62a8e27
Dcse640 <122784540+Dcse640@users.noreply.github.com>
2024-01-28 16:02:48 +0530

    Remove divide Function from Calculator

--- Calculator.c
+++ Calculator.c
@@ -19,18 +19,7 @@ int main() {
         case '-':
             result = num1 - num2;
             break;
-        case '*':
-            result = num1 * num2;
-            break;
-        case '/':
-            // Check if the divisor is not zero
-            if (num2 != 0) {
-                result = num1 / num2;
-            } else {
-                printf("Error: Division by zero is not allowed.\n");
-                return 1; // Exit with an error code
-            }
-            break;
+
         default:
             printf("Error: Invalid operator.\n");
             return 1; // Exit with an error code
